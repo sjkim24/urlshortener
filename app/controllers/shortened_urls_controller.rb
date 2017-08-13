db87d076e6b2c83c18e5b12311693cbb35f9c390
Sung Jin Kim <sjkim0421@gmail.com>
2017-08-13 17:53:42 -0400

    setup shortened_urls controller; add routes to index, create and show controller actions

---
+++ app/controllers/shortened_urls_controller.rb
@@ -0,0 +1,27 @@
+class ShortenedUrlsController < ApplicationController
+  
+  # display top 100 visited links
+  def index
+    
+  end
+  
+  # process original url into a shortened url
+  def create
+    
+  end
+  
+  # for now, i will redirect user to show action after create to simply
+  # show them the shortened url but i prob don't even have to since i 
+  # will be using react for front end
+  def show
+    
+  end
+  
+  # for now, do everything above
+  
+  # PART TWO
+  def redirect_to_original_link
+    
+  end
+  
+end

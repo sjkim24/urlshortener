97606d21bad5fe9a094ab681d6d7d3f27888e23f
Sung Jin Kim <sjkim0421@gmail.com>
2017-08-13 17:46:21 -0400

    add some development gems for better debugging; setup migration to create shortened_urls table; set up model ShortenedUrl

---
+++ db/migrate/20170813214213_create_shortened_ur_ls.rb
@@ -0,0 +1,11 @@
+class CreateShortenedUrLs < ActiveRecord::Migration
+  def change
+    create_table :shortened_urls do |t|
+      t.string :long_url, null: false
+      t.string :short_url, null: false
+      t.integer :visit_count
+    end
+    
+    add_index :shortened_urls, :short_url, unique: true
+  end
+end

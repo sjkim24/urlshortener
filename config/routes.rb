f772eb3c3e97d460592c006ab97c2cde88107a7e
Sung Jin Kim <sjkim0421@gmail.com>
2017-08-20 11:22:08 -0400

    fix routes.rb to properly redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: { format: :json } do
     resources :shortened_urls, only: [:create]
-    get "shortened_urls/:short_url", :to => "shortened_urls#redirect_to_original_link"
   end
+  
+  get "/:short_url", to: "api/shortened_urls#redirect_to_original_link"
 end
